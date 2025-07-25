bdfce40b7a065e0de69a6eb7071e109c6ef4ce54
laulun98 <lpajarinalonso@gmail.com>
2025-07-25 14:19:56 +0200

    Cambios formulario
    Creación de una hoja para javascript y alterado el formulario.

--- prueba.js
+++ prueba.js
@@ -1,3 +1,12 @@
-var miVariable = "freecodecamp";
+function validarForm(){
+    var nombre = document.getElementById("nombre").value;
+    var apellido=document.getElementById("apellido").value;
 
-console.log(miVariable);
+    if(nombre.length == 0){
+        alert("El nombre es obligatorio")
+    } else if (apellido.length==0){
+        alert("Los apellidos son obligatorios")
+    } else {
+        document.getElementById("final").innerHTML=("Formulario validado");
+    }
+}
