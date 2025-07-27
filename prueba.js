6a39a450560fa6a7f570d8c6b753f969a610c891
laulun98 <lpajarinalonso@gmail.com>
2025-07-27 12:01:03 +0200

    Reseteo formulario, mensaje cuando checkbox no es seleccionado

--- prueba.js
+++ prueba.js
@@ -2,13 +2,23 @@ function validarForm(){
     var nombre = document.getElementById("nombre").value;
     var apellido=document.getElementById("apellido").value;
 
+    const formulario = document.getElementById("idForm");
+
+    let elementoActivo = document.querySelector('input[name="fav_lang"]:checked');
+    if(!elementoActivo) {
+        document.getElementById("lengFav").innerHTML=("Selecciona una opción");
+        //alert('Selecciona una opción'); 
+    }
+
     if(nombre.length == 0){
-        document.getElementById("final").innerHTML=("El nombre es obligatorio");
+        document.getElementById("datosPersonales").innerHTML=("El nombre es obligatorio");
         document.getElementById("idNombre").style.color="red";
     } else if (apellido.length==0){
-        document.getElementById("final").innerHTML=("Los apellidos son obligatorios");
+        document.getElementById("datosPersonales").innerHTML=("Los apellidos son obligatorios");
         document.getElementById("idApellido").style.color="red";
     } else {
-        document.getElementById("final").innerHTML=("Formulario validado");
+        document.getElementById("datosPersonales").innerHTML=("Formulario validado");
+        document.getElementById("datosPersonales").style.color="black";
+        formulario.reset();
     }
 }
